9593b3351b7acf3326b3db62c0b5472db85a12f1
Mohit Nayar <mohit.nayar@labatt.com>
2022-05-14 01:25:47 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 pip install --upgrade pip
 pip install requests
 pip install pyyaml
-pip install pandas
 pip install os
 pip install logging
+pip install argparse
+pip install pathlib
 
 python /scripts/python/assign_group.py $1 $2 "$3" $4
