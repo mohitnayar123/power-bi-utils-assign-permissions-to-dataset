31a9268d7f0e76371274966dd4e6ab3c91aeba70
Mohit Nayar <mohit.nayar@labatt.com>
2022-05-14 02:38:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ pip install requests
 pip install pyyaml
 pip install pathlib
 
-python /scripts/python/assign_group.py --files "$1" --tenant_id $2 --config $3
+python /scripts/python/assign_group.py files "$1" tenant_id $2 config $3
