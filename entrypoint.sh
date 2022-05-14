215d3c2e40e6c18fdc94038fcd9e405ff7066655
Mohit Nayar <mohit.nayar@labatt.com>
2022-05-14 00:15:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ pip install pandas
 pip install os
 pip install logging
 
-python /scripts/python/upload_file.py --tenant_id $$1 --config_file $2 --folder $3 --files "$4"
+python /scripts/python/upload_file.py --tenant_id $1 --config_file $2 --folder $3 --files "$4"
